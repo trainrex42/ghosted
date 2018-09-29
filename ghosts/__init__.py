e8603044cd7475426c2638914fa9dc4e5aec98ea
Tanner Stephens <tannerrstephens@gmail.com>
2018-09-29 03:52:03 -0500

    Added ghost icons

--- ghosts/__init__.py
+++ ghosts/__init__.py
@@ -1,10 +1,17 @@
-from flask import Flask, session
+from flask import Flask, session, send_from_directory
 from sqlalchemy_utils import database_exists
 from sqlalchemy.engine.url import make_url
+import os
 
 def create_app(config='ghosts.config.Config'):
   app = Flask(__name__)
   app.config.from_object(config)
+
+  @app.route('/favicon.ico')
+  def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+  
 
   with app.app_context():
     # Database Setup
